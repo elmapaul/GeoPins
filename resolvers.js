a5fd2da12ca54079cb86ef95148b51e8e30de47c
Paul Elmatsidis <p.elmatsidis@gmail.com>
2020-01-05 00:27:24 +0200

    Google OAuth added

--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require("apollo-server");
+
 const user = {
   _id: "1",
   name: "Reed",
@@ -5,8 +7,16 @@ const user = {
   picture: "bla.jpg"
 };
 
+const authenticated = next => (root, args, ctx, info) => {
+  if (!ctx.currentUser) {
+    throw new AuthenticationError("You must be logged in");
+  } else {
+    return next(root, args, ctx, info);
+  }
+};
+
 module.exports = {
   Query: {
-    me: () => user
+    me: authenticated((root, args, ctx) => ctx.currentUser)
   }
 };
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
-const mongoose = require("mongoose");
-
-require("dotenv").config();
+const { findOrCreateUser } = require("./controllers/userController");
 
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -15,7 +15,23 @@ mongoose
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: async ({ req }) => {
+    let authToken = null;
+    let currentUser = null;
+
+    try {
+      authToken = req.headers.authorization;
+
+      if (authToken) {
+        currentUser = await findOrCreateUser(authToken);
+      }
+    } catch (err) {
+      console.error(`Unable to authenticate use with ${authToken}`);
+    }
+
+    return { currentUser };
+  }
 });
 
 server.listen().then(({ url }) => console.log(`Starting on ${url}`));
