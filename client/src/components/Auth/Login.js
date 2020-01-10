5614d846675d2549f5863bf4d613cedf741a9333
Paul Elmatsidis <p.elmatsidis@gmail.com>
2020-01-10 22:00:27 +0200

    Displaying created pins on the map

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -7,6 +7,8 @@ import Typography from "@material-ui/core/Typography";
 
 import Context from "../../context";
 import { ME_QUERY } from "../../graphql/queries";
+import { BASE_URL } from "../../client";
+
 const Login = ({ classes }) => {
   const { dispatch } = useContext(Context);
 
@@ -15,7 +17,7 @@ const Login = ({ classes }) => {
       const idToken = googleUser.getAuthResponse().id_token;
       console.log("id Token: ", idToken);
 
-      const client = new GraphQLClient("http://localhost:4000/graphql", {
+      const client = new GraphQLClient(BASE_URL, {
         headers: { authorization: idToken }
       });
       const { me } = await client.request(ME_QUERY);
